f75364f1f94262d7907e2bc5b43df41ed558f9e3
samuel <samawyck@gmail.com>
2025-01-06 13:32:05 -0600

    SOlve 03_REVERSESTRING

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const letters = [];
+    recursiveReverse(string, letters, string.length - 1);
+    return letters.join("");
 };
+
+function recursiveReverse(string, letters, endIdx) {
+    if (endIdx < 0) {
+        return;
+    }
+
+    let lastLetter = string.charAt(endIdx);
+    letters.push(lastLetter);
+    recursiveReverse(string, letters, endIdx - 1);
+}
 
 // Do not edit below this line
 module.exports = reverseString;
